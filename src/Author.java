59c0428b361f4716086e34eebef36996535331d3
Selda Larsson <selda.larssonsvensdotter@elev.ga.ntig.se>
2024-12-02 17:05:18 +0100

    1

---
+++ src/Author.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Author {
+
+    private String name;
+    private ArrayList<String> Book = new ArrayList<>();
+    private int authorID;
+    private int energy;
+    public String getName;
+
+    public Author(String name, int authorID, int energy){
+        this.name = name;
+        this.Book = Book;
+        this.authorID = authorID;
+        this.energy = energy;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void writeBook(){
+        System.out.println("Namn ge boken");
+
+
+    }
+    public void findBook(){
+
+    }
+    public void rest (int time){
+
+    }
+
+}
---
+++ src/Book.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Book {
+
+    private String title;
+    private int length;
+    private int bookID;
+    private Author author;
+
+    public Book(String title, int length, int bookID) {
+        this.title = title;
+        this.length = length;
+        this.bookID = bookID;
+    }
+}
